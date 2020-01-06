7a635081d3cf4d489b038f0cde6e48d53ec22190
Alexander Yukal <yukal@email.ua>
2020-01-06 16:27:13 +0200

    add Cookie class

---
+++ src/v1/middleware/Cookie.js
@@ -0,0 +1,115 @@
+class Cookie {
+    constructor(component) {
+        Object.defineProperty(this, 'component', {
+            value: component
+        });
+    }
+
+    getItems(hostname, callback) {
+        const items = {};
+
+        if (hostname !== undefined) {
+            if (this.hasOwnProperty(hostname)) {
+                const entry = this[ hostname ];
+                const data = Object.keys(entry).map(key => entry[key].value);
+
+                return callback ?callback(data, hostname) :data;
+            }
+            return undefined;
+        } else {
+            for (const host in this) {
+                const entry = this[ host ];
+                const data = Object.keys(entry).map(key => entry[key].value);
+                items[ host ] = callback ?callback(data, host) :data;
+            }
+        }
+
+        return items;
+    }
+
+    getValues(hostname) {
+        return this.getItems(hostname, data => data.join('; '));
+    }
+
+    update(cookieData, hostname) {
+        // const hostname = host.replace(/^www\./i, '');
+        if (hostname && Array.isArray(cookieData)) {
+            const parsedCookie = Cookie.parse(cookieData);
+            this[ hostname ] = this.hasOwnProperty(hostname)
+                ? updateCookie(this[hostname], parsedCookie)
+                : parsedCookie
+            ;
+        }
+
+        else if (cookieData instanceof Cookie) {
+            for (const item in cookieData) {
+                this[item] = cookieData[item];
+            }
+        }
+
+        else if (cookieData !== null && typeof cookieData == 'object') {
+            Object.assign(this, cookieData);
+        }
+    }
+}
+
+Cookie.parse = function parseCookie(data) {
+    const cookiesCollection = typeof(data) == 'string' ?[ data ] :data;
+    const today = Date.now();
+    const parsed = {};
+
+    for (const param of cookiesCollection) {
+        const chunks = param.split(';');
+        const value = chunks.shift().trim();
+        const ID = value.substr(0, value.indexOf('='));
+        const cookie = { value };
+
+        chunks.map(item => {
+            const [ key, value ] = item.trim().split('=');
+            const lowKey = key.toLowerCase();
+
+            lowKey === 'expires'
+                ? cookie[ lowKey ] = Date.parse(value)
+                : cookie[ lowKey ] = value || true
+            ;
+        });
+
+        if (cookie.hasOwnProperty('expires')) {
+            if (today > Date.parse(cookie.expires)) {
+                continue;
+            }
+        }
+
+        if (! parsed.hasOwnProperty(ID)) {
+            parsed[ID] = cookie;
+        } else if (cookie.hasOwnProperty('expires') && parsed[ID].hasOwnProperty('expires')) {
+            cookie.expires > parsed[ID].expires && (parsed[ID] = cookie);
+        }
+    }
+
+    return parsed;
+}
+
+function updateCookie(oldCookie, newCookie) {
+    const cookie = Object.assign({}, oldCookie);
+
+    for (const key in newCookie) {
+        let canUpdate = false;
+
+        if (!oldCookie.hasOwnProperty(key)) {
+            canUpdate = true;
+        } else if (oldCookie[key].hasOwnProperty('expires') && newCookie[key].hasOwnProperty('expires')) {
+            if (oldCookie[key].expires < newCookie[key].expires) {
+                canUpdate = true;
+            }
+        }
+
+        if (canUpdate) {
+            cookie[ key ] = newCookie[ key ];
+        }
+    }
+
+    return cookie;
+}
+
+module.exports = Cookie;
