b707eab40044ab9e7dff5628adafdef445aaaa7b
Alexander Yukal <yukal@email.ua>
2020-01-06 16:28:33 +0200

    update Brequest

--- src/v1/middleware/Brequest.js
+++ src/v1/middleware/Brequest.js
@@ -3,6 +3,7 @@ const https = require('https');
 const http = require('http');
 const zlib = require('zlib');
 const iconv = require('iconv-lite');
+const Cookie = require('./Cookie');
 // iconv.extendNodeEncodings();
 const { isObject, cloneObject } = require('./datas');
 
@@ -18,11 +19,20 @@ const decoders = {
 
 class Brequest {
     constructor() {
-        // this.cookies = {};
+        let cookies = new Cookie(this);
+
         Object.defineProperties(this, {
             cookies: {
-                get: () => getCookies.call(this),
-                set: val => setCookies.call(this, val),
+                // value: new Cookie()
+                enumerable: true,
+                get: () => cookies,
+                set: (val) => {
+                    if (val === null) {
+                        cookies = new Cookie();
+                    } else {
+                        cookies.update(val);
+                    }
+                }
             },
         });
     }
@@ -48,29 +58,39 @@ class Brequest {
         const options = getOptions('POST', url, headers);
         return request.call(this, options, JSON.stringify(data));
     }
-}
 
-Brequest.prototype.encodeURI = function encodeURI(obj) {
-    let uriChunks = [];
-    for (const key in obj) {
-        const data = obj[key] !== null && typeof obj[key] == 'object'
-            ? JSON.stringify(obj[key]) 
-            : obj[key]
-        ;
-        const uriChunk = key + '=' + encodeURIComponent(data);
-        uriChunks.push(uriChunk);
+    getHeaders(entry) {
+        if (this.hasOwnProperty('res')) {
+            return entry ?this.res.headers[entry] :this.res.headers;
+        }
+        return undefined;
     }
-    return uriChunks.join('&');
+
+    getHostname(hostname) {
+        const host = hostname ?hostname :this.requestOptions.host;
+        return host.replace(/^www\./i, '');
+    }
+
+    encodeURI(obj) {
+        let uriChunks = [];
+        for (const key in obj) {
+            const data = obj[key] !== null && typeof obj[key] == 'object'
+                ? JSON.stringify(obj[key]) : obj[key];
+            const uriChunk = key + '=' + encodeURIComponent(data);
+            uriChunks.push(uriChunk);
+        }
+        return uriChunks.join('&');
+    }
 }
 
 function request(options, data) {
     const self = this;
     const RequestModule = options.protocol === 'https:'? https: http;
-    const host = getHostFrom(options.host);
+    const host = this.getHostname(options.host);
     let postData = null;
 
     if (this.cookies.hasOwnProperty(host)) {
-        options.headers.Cookie = getCookieValues.call(this, host).join('; ');
+        options.headers.Cookie = this.cookies.getValues(host);
     }
 
     if (data) {
@@ -121,21 +141,15 @@ function request(options, data) {
     });
 }
 
-request.COOKIE_KEY = '__cookies';
-
 request.finish = function(err, bodyBuff, resolve, reject) {
-    // const reporters = this.reporters || [];
-
     if (err) {
         console.error(err);
-        return reject(null);
+        return reject(err);
     }
 
     if (this.res.headers.hasOwnProperty('set-cookie')) {
-        const host = getHostFrom(this.requestOptions.host);
-        const cookies = this.res.headers['set-cookie'];
-        this.cookies = { host, cookies };
-        console.log(getCookieValues.call(this, host));
+        this.cookies.update(this.res.headers['set-cookie'], this.getHostname());
+        // console.log(this.cookies.getItems(host));
     }
 
     // if (this.res.statusMessage != 'OK')
@@ -190,137 +204,6 @@ function decodeBody(buff, contentType) {
     }
 
     return body.toString();
-}
-
-function getHostFrom(host) {
-    return host.replace(/^www\./i, '');
-}
-
-function getCookieValues(host) {
-    const cookies = this.cookies[ host ] || {};
-    return Object.keys(cookies).map(key => cookies[key].value);
-}
-
-function parseCookies(cookies) {
-    const cookiesCollection = typeof(cookies) == 'string' ?[ cookies ] :cookies;
-    const today = Date.now();
-    const parsed = {};
-
-    for (const param of cookiesCollection) {
-        const chunks = param.split(';');
-        const value = chunks.shift().trim();
-        const ID = value.substr(0, value.indexOf('='));
-        const cookie = { value };
-
-        chunks.map(item => {
-            const [ key, value ] = item.trim().split('=');
-            const lowKey = key.toLowerCase();
-
-            lowKey === 'expires'
-                ? cookie[ lowKey ] = Date.parse(value)
-                : cookie[ lowKey ] = value || true
-            ;
-        });
-
-        if (cookie.hasOwnProperty('expires')) {
-            if (today > Date.parse(cookie.expires)) {
-                continue;
-            }
-        }
-
-        if (! parsed.hasOwnProperty(ID)) {
-            parsed[ID] = cookie;
-        } else if (cookie.hasOwnProperty('expires') && parsed[ID].hasOwnProperty('expires')) {
-            cookie.expires > parsed[ID].expires && (parsed[ID] = cookie);
-            // Date.parse(cookie.expires) > Date.parse(parsed[ID].expires) && (parsed[ID] = cookie);
-        }
-    }
-
-    return parsed;
-}
-
-function mergeCookies() {
-    const args = [ ...arguments ];
-
-    if (args.length <= 1) {
-        return args.shift();
-    }
-
-    const cookies = args.shift();
-
-    args.map(items => {
-        Object.keys(items).map(ID => {
-            if (! cookies.hasOwnProperty(ID)) {
-
-                cookies[ID] = items[ID];
-
-            } else if (cookies[ID].hasOwnProperty('expires') && items[ID].hasOwnProperty('expires')) {
-
-                items[ID].expires > cookies[ID].expires && (cookies[ID] = items[ID]);
-
-            }
-        });
-    });
-
-    return cookies;
-}
-
-function getCookies() {
-    if (! this.hasOwnProperty(request.COOKIE_KEY)) {
-        initCookies.call(this);
-    }
-    return this[request.COOKIE_KEY];
-}
-
-function setCookies(data) {
-    if (data === null) {
-        initCookies.call(this);
-    } else {
-        const { host, cookies } = data;
-        const instance = this.hasOwnProperty(request.COOKIE_KEY) 
-            ? this[ request.COOKIE_KEY ] : initCookies.call(this);
-
-        if (! instance.hasOwnProperty(host)) {
-            instance[ host ] = {};
-        }
-
-        instance[host] = mergeCookies(instance[host], parseCookies(cookies));
-    }
-}
-
-function initCookies(target = request.COOKIE_KEY) {
-    const instance = (this[target] = {});
-
-    return Object.defineProperties(this[target], {
-        // Returns a string with values separated by ";"
-        values: {
-            get: function() {
-                const cookiesList = {};
-
-                for (const host in instance) {
-                    const cookies = instance[ host ];
-                    const items = Object.keys(cookies).map(key => cookies[key].value);
-                    cookiesList[ host ] = items.join('; ');
-                }
-
-                return cookiesList;
-            }
-        },
-
-        // Returns an array with values
-        items: {
-            get: function() {
-                const cookiesList = {};
-
-                for (const host in instance) {
-                    const cookies = instance[ host ];
-                    cookiesList[ host ] = Object.keys(cookies).map(key => cookies[key].value);
-                }
-
-                return cookiesList;
-            }
-        }
-    });
 }
 
 module.exports = Brequest;
