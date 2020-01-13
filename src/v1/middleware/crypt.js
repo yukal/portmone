69bf2c291e841745d2bee139754938760d323e85
Alexander Yukal <yukal@email.ua>
2020-01-14 00:42:01 +0200

    update crypt

--- src/v1/middleware/crypt.js
+++ src/v1/middleware/crypt.js
@@ -1,15 +1,13 @@
 const crypto = require('crypto');
 const aes = require('./AesUtil');
 const rsa = require('./RsaUtil');
-const datas = require('./datas');
+const { randomInt } = require('./datas');
 
-const CHR_LET = 0;
-const CHR_DIG = 2;
-const CHR_CHR = 4;
-
-const numbers = '0123456789';
-const special = '^~@#$&%!?*-+[_=](.:)<,;>/|';
-const letters = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ';
+const CHR_LOCASE = 1;
+const CHR_UPCASE = 2;
+const CHR_WORD = 3; // CHR_LOW|CHR_UP
+const CHR_DIGIT = 4;
+const CHR_SPEC = 8;
 
 function md5() {
     const text = Array.prototype.slice.call(arguments).join(':');
@@ -17,6 +15,20 @@ function md5() {
 }
 
 function crc32(str) {
+    function makeCRC32Table() {
+        let c, crcTable = [];
+
+        for (let n=0; n<256; n++) {
+            c = n;
+            for (let k=0; k<8; k++) {
+                c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+            }
+            crcTable[ n ] = c;
+        }
+
+        return crcTable;
+    }
+
     const crcTable = window.crcTable || (window.crcTable = makeCRC32Table());
     let crc = 0 ^ (-1);
 
@@ -27,85 +39,65 @@ function crc32(str) {
     return (crc ^ (-1)) >>> 0;
 };
 
-function makeCRC32Table() {
-    let c, crcTable = [];
-
-    for (let n=0; n<256; n++) {
-        c = n;
-        for (let k=0; k<8; k++) {
-            c = ((c&1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
-        }
-        crcTable[ n ] = c;
+function genPassword(length=32, mode=CHR_WORD|CHR_DIGIT|CHR_SPEC) {
+    if (!(Number.isInteger(mode) && mode>0)) {
+        return false;
     }
 
-    return crcTable;
-}
+    const ASCII = [
+        // Special chars: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        // 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A,
+        // 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E,
+        // 0x3F, 0x40, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F, 0x60, 0x7B, 0x7C,
+        // 0x7D, 0x7E,
 
-function generatePassword(length=32) {
-    const numbers = '0123456789';
-    const special = '^~@#$&%!?*-+[_=](.:)<,;>/|';
-    const letters = 'aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ';
+        // Special chars (0-16): !#$%&()*,.:;@[]^~+|
+        0x21, 0x23, 0x24, 0x25, 0x26, 0x28, 0x29, 0x2A, 0x2C, 0x2E,
+        0x3A, 0x3B, 0x40, 0x5B, 0x5D, 0x5E, 0x7E, 0x2B, 0x7D,
 
-    const qtys = Math.floor((datas.randomInt(10, 20) * length) / 100) || 1;
-    const qtyn = Math.floor((datas.randomInt(10, 30) * length) / 100) || 1;
-    const qtyc = length - qtyn - qtys;
+        // Digits [0..9]
+        0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
 
-    let pass = generatePasswordChars(letters, qtyc);
-    pass = appendPasswordChars(pass, generatePasswordChars(special, qtys));
-    pass = appendPasswordChars(pass, generatePasswordChars(numbers, qtyn));
+        // Lower case letters [A..Z]
+        0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A,
+        0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54,
+        0x55, 0x56, 0x57, 0x58, 0x59, 0x5A,
 
-    return pass;
-}
+        // Upper case letters [a..z]
+        0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A,
+        0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74,
+        0x75, 0x76, 0x77, 0x78, 0x79, 0x7A
+    ];
 
-function generatePasswordChars(chars, num=4) {
-    let word = '';
-    for (let x=0, len=chars.length; x<num; x+=1) {
-        word += chars[ datas.randomInt(0, len-1) ];
-    }
-    return word;
-}
+    let map = [];
 
-function appendPasswordChars(pass, chars) {
-    let phrase = pass;
+    // Fill map
+    ((mode & CHR_SPEC)   === CHR_SPEC)   && (map = map.concat(ASCII.slice(0,19)));
+    ((mode & CHR_DIGIT)  === CHR_DIGIT)  && (map = map.concat(ASCII.slice(19,29)));
+    ((mode & CHR_LOCASE) === CHR_LOCASE) && (map = map.concat(ASCII.slice(29,55)));
+    ((mode & CHR_UPCASE) === CHR_UPCASE) && (map = map.concat(ASCII.slice(55,81)));
 
-    for (let n=0, plen=phrase.length, vlen=chars.length; n<vlen; n+=1) {
-        const index = datas.randomInt(1, plen-1);
-        phrase = phrase.substr(0, index) + chars[ n ] + phrase.substr(index);
-        plen += 1;
-    }
+    // const specialCharsLimit = Math.ceil(length*12/100);
+    const mapLength = map.length;
+    const arr = Array.from({ length }, () => map[randomInt(0, mapLength-1)]);
 
-    return phrase;
-}
-
-function genChars(num=4, voc=CHR_LET|CHR_DIG) {
-    const vocabularies = [];
-    let word = '';
-
-    (voc & CHR_LET) === 0 && vocabularies.push(letters);
-    (voc & CHR_DIG) === 2 && vocabularies.push(numbers);
-    (voc & CHR_CHR) === 4 && vocabularies.push(special);
-
-    for (let x=0; x<num; x+=1) {
-        let vocabulary = vocabularies[ datas.randomInt(0, vocabularies.length-1) ];
-        word += vocabulary[ datas.randomInt(0, vocabulary.length-1) ];
-    }
-
-    return word;
+    return Buffer.from(arr).toString('ascii');
 }
 
 module.exports.aes = aes;
 module.exports.rsa = rsa;
 module.exports.md5 = md5;
 module.exports.crc32 = crc32;
-module.exports.generatePassword = generatePassword;
+module.exports.genPassword = genPassword;
 
 module.exports = {
-    CHR_LET,
-    CHR_DIG,
-    CHR_CHR,
+    CHR_UPCASE,
+    CHR_LOCASE,
+    CHR_WORD,
+    CHR_DIGIT,
+    CHR_SPEC,
 
-    genChars,
-    generatePassword,
+    genPassword,
     crc32,
     md5,
     aes,
