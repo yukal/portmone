4189cdcf21bb32d777d48017aabf8320608050e1
Alexander Yukal <yukal@email.ua>
2020-01-14 00:43:09 +0200

    update bill controller

--- src/v1/controllers/bill.js
+++ src/v1/controllers/bill.js
@@ -2,53 +2,37 @@ const crypto = require('crypto');
 const zlib = require('zlib');
 const util = require('util');
 const fs = require('fs');
+
 const readFile = util.promisify(fs.readFile);
 const fileExists = util.promisify(fs.exists);
+const gunzip = util.promisify(zlib.gunzip);
+const gzip = util.promisify(zlib.gzip);
+const randomBytes = util.promisify(crypto.randomBytes);
 
 const { fail, done } = require('../middleware/process');
 const config = require('../../config.json');
 const crypt = require('../middleware/crypt');
 const datas = require('../middleware/datas');
 const colors = require('../middleware/colors');
-const formatter = require('../middleware/formatter');
+const Formatter = require('../middleware/formatter');
 const PortmoneAPI = require('../middleware/PortmoneAPI');
 const CACHE_DIR = './data/cache';
 
 let API;
+config.onApiData = onApiData;
+config.onApiError = onApiError;
 
 async function rtBill(req, res) {
-    const { secret, authKey, MM, YY, cvv2, card_number } = req.body;
-    let CCARD;
-
     if (! validateBillBody(req.body)) {
         return fail(res, 'Wrong parameters');
-    }
-
-    if (req.body.hasOwnProperty('card_number')) {
-
-        CCARD = { MM, YY, cvv2, card_number };
-
-    } else {
-        const data = await loadData(authKey);
-        if (! await verifyAuth(data, authKey, config)) {
-            return fail(res, 'Wrong auth key');
-        }
-
-        const jsonData = crypt.aes.decrypt(data.data, data.secret);
-        if (! datas.isJSON(jsonData)) {
-            return fail(res, 'Wrong card data');
-        }
-
-        CCARD = JSON.parse(jsonData);
     }
 
     const currency = 'UAH';
     const amount = Number.parseInt(req.body.amount, 10) || 0;
     const phone = datas.parseMobilePhone(req.body.phone);
+    const CCARD = await loadCreditCardData(req.body);
 
     API = new PortmoneAPI(config);
-    API.on('api-error', onApiError);
-    API.on('api-data', onApiData);
 
     API.bill(currency, amount, phone, CCARD)
         .then(data => done(res, data))
@@ -63,15 +47,19 @@ function rtCheckPin(req, res) {
         return fail(res, 'Wrong parameters');
     }
 
+    if (!API) {
+        API = new PortmoneAPI(config);
+    }
+
     API.checkPin(pin)
-        .then(response => {
+        .then(() => {
             const { name, lat, lng } = API.location;
             const statusMsg = util.format('%s Payment successful!', colors.green('✔'));
 
             console.log('  %s\n    %s  (%s, %s)', statusMsg, name, lat, lng);
             done(res);
         })
-        .catch(response => {
+        .catch(() => {
             console.log('  %s Payment failed!', colors.red('✖'));
             fail(res, 'unknown error');
         })
@@ -79,29 +67,49 @@ function rtCheckPin(req, res) {
 }
 
 async function rtEncode(req, res) {
-    if (!req.body.hasOwnProperty('card_number_mask')) {
-        req.body.card_number_mask = datas.getMaskCardN16(req.body.card_number);
+    const { MM, YY, cvv2, card_number } = req.body;
+    const creditCardDigits = util.format('%s%s%s%s', card_number, cvv2, MM, YY);
+    const bufBytes = datas.digitsToBytes(creditCardDigits);
+
+    const encoding = 'binary';
+    const noiseLen = 512 - bufBytes.length - 1;
+    const offset = datas.randomInt(0, 255);
+    const offsetBuff = Buffer.from([offset]);
+
+    const secret = crypt.genPassword(32);
+    const file = util.format('%s/%s', CACHE_DIR, crypt.md5(secret));
+
+    try {
+
+        const noise = await randomBytes(noiseLen);
+        const bufNoised = Buffer.concat([
+            offsetBuff,
+            noise.slice(0, offset),
+            bufBytes,
+            noise.slice(offset)
+        ]);
+
+        const bufGzipped = await gzip(bufNoised);
+        const bufEncrypted = crypt.aes.encrypt(bufGzipped.toString(encoding), secret, false);
+
+        fs.writeFile(file, bufEncrypted, encoding, function(err) {
+            err ? console.error(err) 
+                : console.log('Saved: "%s" [%d b]', file, bufEncrypted.length)
+            ;
+        });
+
+    } catch(err) {
+
+        console.error(err);
+
     }
 
-    const secret = crypt.generatePassword(32);
-    const data = crypt.aes.encrypt(JSON.stringify(req.body), secret);
-
-    const { encType, encOpts } = datas.cloneObject(config.crypt);
-    encOpts.privateKeyEncoding.passphrase = secret;
-
-    const { publicKey, privateKey } = crypto.generateKeyPairSync(encType, encOpts);
-    const pbKeyB64 = await crypt.rsa.encompress(publicKey, encOpts.publicKeyEncoding.type, 'base64');
-
-    const jsonData = JSON.stringify({ secret, data, privateKey }, null, 4);
-    const destination = util.format('%s/%s', CACHE_DIR, crypt.md5(pbKeyB64));
-    fs.writeFile(destination, jsonData, err => err && console.error(err));
-
-    done(res, { authKey: pbKeyB64 });
+    done(res, { authKey: secret });
 }
 
 async function rtDecode(req, res) {
     // const { secret, authKey } = req.body;
-    // const data = await loadData(authKey);
+    // const data = await loadCreditCardData(authKey);
 
     // if (! await verifyAuth(data, authKey, config)) {
     //     return fail(res, 'Wrong auth key');
@@ -159,117 +167,107 @@ async function verifyAuth(data, key, config, decode='base64') {
     // const check = crypt.rsa.verifySignature(privateKey, publicKey, secret);
 }
 
-async function loadData(authKey) {
+async function loadCreditCardData(body) {
+    const { secret, authKey, MM, YY, cvv2, card_number } = body;
     const destination = util.format('%s/%s', CACHE_DIR, crypt.md5(authKey));
     let data = false;
 
+    // const isRawData = body.hasOwnProperty('card_number') 
+    //     && body.hasOwnProperty('MM') 
+    //     && body.hasOwnProperty('YY') 
+    //     && body.hasOwnProperty('cvv2')
+    // ;
+    // if (isRawData) {
+    //     return { MM, YY, cvv2, card_number };
+    // }
+
+    if (MM && YY && cvv2 && card_number) {
+        return { MM, YY, cvv2, card_number };
+    }
+
     if (!authKey) {
+        console.error(arguments.callee.name, 'No auth key');
         return false;
     }
 
     if (!await fileExists(destination)) {
+        console.error(arguments.callee.name, `File doesnt exists: "${destination}"`);
         return false;
     }
 
     try {
-        const jsonData = await readFile(destination, 'utf8');
-        data = JSON.parse(jsonData);
-    } catch (err) {
-        // console.error(err);
+
+        const buffer = await readFile(destination);
+        const cryptedString = crypt.aes.decrypt(buffer, authKey);
+        const cryptedBuffer = Buffer.from(cryptedString, 'binary');
+
+        const unzippedBuffer = await gunzip(cryptedBuffer);
+        const offset = unzippedBuffer[0] + 1;
+        const chunk = unzippedBuffer.slice(offset, offset+12);
+        data = datas.getCardDatas(chunk);
+        // console.log(datas.bytesToDigits(chunk), data);
+
+    } catch(err) {
+
+        console.error(err);
+
     }
 
     return data;
 }
 
 function onApiError(err) {
-    process.stdout.write(colors.red(arguments.callee.name) + colors.mono(6, ' => '));
-    const dumpfile = util.format('%s/%s.dump', CACHE_DIR, this.currScenarioAlias);
+    // process.stdout.write(colors.red(arguments.callee.name) + colors.mono(6, ' => '));
+    process.stdout.write(colors.red(this.currScenario.name) + colors.mono(6, ' => '));
+    console.error(err.toString());
 
-    if (datas.isObject(err)) {
-        if (err.hasOwnProperty('msg')) {
-            // const { res, options, body, parsedData } = err.response;
-            const textBody = JSON.stringify({
-                headers: this.client.res.headers,
-                href: this.client.requestOptions.href,
-                data: this.client.requestData,
-                // body: this.client.res.,
-            }, null, 4);
+    const dumpData = {
+        error_string: err.toString(),
+        error_object: err,
+        href: this.client.requestOptions.href,
+        headers: this.client.res ?this.client.res.headers :typeof(this.client.res),
+        data: this.client.requestData,
+        body: this.client.body.toString(),
+    };
 
-            fs.writeFile(dumpfile, textBody, 'utf8', err => err && console.error(err));
-            console.error(err.msg);
-        } else {
-            const textBody = JSON.stringify(err, null, 4);
-            fs.writeFile(dumpfile, textBody, 'utf8', err => err && console.error(err));
-            console.error(err);
-        }
-        // err.hasOwnProperty('msg') ?console.error(err.msg) :console.error(err);
-    } else {
-        const textBody = JSON.stringify(err, null, 4);
-        fs.writeFile(dumpfile, textBody, 'utf8', err => err && console.error(err));
-        console.error(err);
+    if (datas.isEmpty(dumpData.error_object)) {
+        delete dumpData.error_object;
     }
+
+    const content = JSON.stringify(dumpData, null, 4);
+    const dumpfile = util.format('%s/%s.dump', CACHE_DIR, this.currScenarioAlias);
+    fs.writeFile(dumpfile, content, 'utf8', er => er && console.error(er));
 }
 
-function onApiData(data, response) {
-    const indentWidth = 4;
-    const frameWidth = process.stdout.columns - indentWidth * 2;
-
+function onApiData(data) {
     if (!onApiData.dumpfile) {
         onApiData.dumpfile = util.format('%s/%s', CACHE_DIR, Date.now());
     }
 
     try {
-        const headerColors = {
-            href: 'gray',
-            method: 'white',
-            status: function paintStatusCode(val) {
-                if (val>199 && val<300) { return 'green'; }
-                else if (val>299 && val<400) { return 'brown'; }
-                else if (val>399) { return 'red'; }
-                return 'gray';
-            },
-            'content-type': '029',
-            // server: 'gray',
-        };
-
+        // const dumpfile = util.format('%s-%s', onApiData.dumpfile, this.currScenarioAlias);
+        const dumpfile = util.format('%s/%s', CACHE_DIR, this.currScenarioAlias);
         const values = data.hasOwnProperty('values') ?data.values :data;
-        const textHeadersColored = formatter.headerData(this, headerColors)
-            + '\n' + datas.tree(values, indentWidth, frameWidth, treeCallback) 
-            + '\n\n'
-        ;
+        const fmt = new Formatter({ api:this, indentWidth:4 });
 
-        const textHeaders = formatter.headerData(this)
-            + '\n' + datas.tree(values, indentWidth, frameWidth) 
-            + '\n\n'
-        ;
+        fmt.log(values);
+        fmt.dump(`${onApiData.dumpfile}.log`, values);
 
-        const dumpfile = util.format('%s-%s', onApiData.dumpfile, this.currScenarioAlias);
-        const textBody = typeof(response.body) == 'string' 
-            ? response.body 
-            : JSON.stringify(response.body, null, 4)
-        ;
+        let body = this.client.body;
+        body = typeof(body)=='string' ?body :JSON.stringify(body, null, 4);
 
-        // fs.writeFile(dumpfile, textBody, 'utf8', err => err && console.error(err));
-        zlib.gzip(Buffer.from(textBody), (_, result) => {
+        // fs.writeFile(dumpfile, body, 'utf8', err => err && console.error(err));
+        zlib.gzip(Buffer.from(body), (_, result) => {
             fs.writeFile(`${dumpfile}.gz`, result, 'binary', e=>e&&console.error(e))
         });
-
-        fs.appendFile(`${onApiData.dumpfile}.log`, textHeaders, 'utf8', err => err && console.error(err));
-        process.stdout.write(textHeadersColored);
 
     } catch (err) {
         console.error(arguments.callee.name, err);
     }
 
-    if (this.currScenarioAlias == this.DONE) {
+    if (this.currScenarioAlias == PortmoneAPI.DONE) {
         onApiData.dumpfile = false;
     }
-}
-
-function treeCallback(val, key) {
-    return colors('023', key.toUpperCase()) + colors.gray(`: ${val}`);
-    // return colors('244', key.toUpperCase()) + colors.gray(`: ${val}`);
-    // return colors('106', key.toUpperCase()) + colors.gray(`: ${val}`);
 }
 
 module.exports = {
