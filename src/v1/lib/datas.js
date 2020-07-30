7917d8c59c9d3b7076281179bf3069ebc94f79bf
Alexander <yukal@email.ua>
2020-07-30 16:52:28 +0300

    fix digits converter
    - fix digitsToBytes
    - fix bytesToDigits

--- src/v1/lib/datas.js
+++ src/v1/lib/datas.js
@@ -299,32 +299,69 @@ function tree(obj, indent=0, limit=86, callbacks={}) {
     return data.join('\n');
 }
 
+/**
+ * digitsToBytes
+ * Encompresses digits to the bytes and represents it as a Buffer data.
+ * It ties up each pair of two digits and converts it to ASCII byte codes 
+ * in numeric format.
+ * @see bytesToDigits
+ *
+ * @param {string} str String with digits only
+ * @returns {buffer} Buffer
+ */
 function digitsToBytes(str) {
-    const chunks = str.replace(/[^\d]/g,'').match(/\d{1,2}/g);
-    let chars = '';
+    const buff = str
+        // Remove all except digits
+        .replace(/\D/g, '')
 
-    for (let digit of chunks) {
-        if (digit < 10) {
-            digit += 100;
-        }
-        digit = String.fromCharCode(digit);
-        chars = `${chars}${digit}`;
-    }
+        // Ties up the paired stringed numbers and returns it as an array
+        // example: 01234 => ['01','23','4']
+        .match(/\d{1,2}/g)
 
-    return Buffer.from(chars, 'ascii');
+        // Converts string chunks to digits (including a leading zero).
+        // If you take a look at two different arrays you will see the same result as 
+        // buffered data representation but for the string data it would be different 
+        // data, and when you will try to decode it, it would give the wrong data.
+        // So thats because the number with a leading zero should start from 100
+        // 
+        // const buff1 = [
+        //     '0', '1', '2', '3',
+        //     '4', '5', '6', '7',
+        //     '8', '9', '10'
+        // ]
+        // 
+        // const buff2 = [
+        //     '00', '01', '02',
+        //     '03', '04', '05',
+        //     '06', '07', '08',
+        //     '09', '10'
+        // ]
+        // 
+        // buff1.join('')  "012345678910"
+        // buff2.join('')  "0001020304050607080910"
+        // 
+        // Buffer.from(buff1)  <Buffer 00 01 02 03 04 05 06 07 08 09 0a>
+        // Buffer.from(buff2)  <Buffer 00 01 02 03 04 05 06 07 08 09 0a>
+
+        .map(n => n.length==2 && n<10 ? +n+100 :+n)
+    ;
+
+    return Buffer.from(buff);
 }
 
+/**
+ * bytesToDigits
+ * Decompresses buffer encoded by digitsToBytes method
+ * @see digitsToBytes
+ *
+ * @param {buffer} buff Buffer
+ * @returns {string} Digits in a string representation
+ */
 function bytesToDigits(buff) {
     let digits = '';
 
-    for (let byte of buff) {
-        if (byte >= 200) {
-            byte = `0${byte - 200}`;
-        }
-        else if (byte >= 100) {
-            byte -= 100;
-        }
-        digits = `${digits}${byte}`;
+    for (const n of buff) {
+        digits += n > 99 ?`0${n-100}` :`${n}`;
     }
 
     return digits;
